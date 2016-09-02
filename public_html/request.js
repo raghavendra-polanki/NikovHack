317f75b4f7e43dbd8c96cc566179f2ef9e63794d
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2016-09-02 19:40:38 +0530

    Google vision api request successful. Response data ready to be sanitized.

---
+++ public_html/request.js
@@ -0,0 +1,34 @@
+function makeRequest() {
+    var request = gapi.client.vision.images.annotate({
+      "requests":
+      [
+        {
+          "image":
+          {
+            "content": $('#photo').attr('src').replace("data:image/jpeg;base64,", "")
+          },
+          "features":
+          [
+            {
+              "maxResults": 5,
+              "type": "TEXT_DETECTION"
+            }
+          ]
+        }
+      ]
+    });
+    request.then(function(response) {
+        sanitize(response);
+        }, function(reason) {
+            console.log('Error: ' + reason.result.error.message);
+        }
+    );
+    // console.log("Loaded");
+}
+
+function init() {
+    gapi.client.setApiKey('AIzaSyAPHpiTbAzrx8_BmyuitgkJpEvj8JibCJc');
+    gapi.client.load('vision').then(makeRequest);
+}
+
+function hitAPI() { gapi.load('client', init); }
---
+++ public_html/sanitize.js
@@ -0,0 +1,3 @@
+function sanitize (data) {
+    console.log(data);
+}
